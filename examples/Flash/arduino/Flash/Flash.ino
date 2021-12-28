45ebefa6a08493d7e61411f5d1147dfcd5f17779
Jonas Scharpf <jonas@brainelectronics.de>
2021-12-28 17:27:49 +0100

    add dummy arduino external flash usage example

---
+++ examples/Flash/arduino/Flash/Flash.ino
@@ -0,0 +1,23 @@
+/*
+  External Flash example
+
+  Format the external flash and create a directory "external" on it.
+  Show all files in the device root directory and save a file named
+  "some-file.txt" to the external flash.
+  Unmount the external flash and show again all files in the device root dir.
+  Mount the external flash again, show all files in the mounted directory and
+  print the content of the previously saved file
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
